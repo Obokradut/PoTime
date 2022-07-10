4ab85d918c15fcf357e20e78ea72465e37de904c
Obokradut <Visond@rambler.ru>
2022-07-10 19:49:27 +0700

    Добавьте файлы проекта.

---
+++ CoolTimer/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace CoolTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _startTimer = false;
+        private bool _isChill = false;
+
+
+        private int _hours = 1;
+        private int _minutes = 0;
+        private int _seconds = 0;
+    
+
+        private int increment = 0;
+        private DispatcherTimer _timer = new DispatcherTimer();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            increment = (_hours * 60 * 60) + (_minutes * 60) + _seconds;
+            durationTimer.Text = $"{_hours:00}:{_minutes:00}:{_seconds:00}";
+
+            _timer.Tick += timerTick;
+            _timer.Interval = TimeSpan.FromSeconds(1);
+        }
+
+        private void timerTick(object? sender, EventArgs e)
+        {
+            increment--;
+
+            _seconds--;
+            if(_seconds == -1)
+            {
+                _minutes--;
+                _seconds = 59;
+            }
+            if (_minutes == -1)
+            {
+                _hours--;
+                _minutes = 59;
+            }
+
+            if (_hours == -1)
+            {
+                if (!_isChill)
+                {
+                    _isChill = !_isChill;
+                    startChill();
+                }
+                else
+                {
+                    _isChill = !_isChill;
+                    startWorking();
+                }                    
+            }
+                
+            
+
+            durationTimer.Text = $"{_hours:00}:{_minutes:00}:{_seconds:00}";
+        }
+
+        private void startWorking()
+        {
+            MessageBox.Show("Время отдыха окончено", "Опа", MessageBoxButton.OK);
+            _hours = 1;
+            _minutes = 0;
+            _seconds = 0;
+        }
+
+        private void startChill()
+        {
+            MessageBox.Show("Время работы окончено", "Опа", MessageBoxButton.OK);
+            _hours = 0;
+            _minutes = 20;
+            _seconds = 0;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void startStopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (_startTimer)
+            {
+                case false: _startTimer = true; _timer.Start(); break;
+                case true: _startTimer = false; _timer.Stop(); break;
+            }
+        }
+
+        private void durationTimer_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isChill = false;
+            _startTimer = false;
+            _timer.Stop();
+            _hours = 1;
+            _minutes = 0;
+            _seconds = 0;
+            durationTimer.Text = $"{_hours:00}:{_minutes:00}:{_seconds:00}";
+        }
+    }
+}
---
+++ CoolTimer/TimerModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CoolTimer
+{
+    public class TimerModel
+    {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+        private object? _backgroundMusic;
+
+
+        public int Hours
+        {
+            get { return _hours; }
+            set { _hours = value; }
+        }          
+        public int Minutes
+        {
+            get { return _minutes; }
+            set { _minutes = value; }
+        }
+        public int Seconds
+        {
+            get { return _seconds; }
+            set { _seconds = value; }
+        }
+        public object? BackgroundMusic
+        {
+            get { return _backgroundMusic; }
+            set { _backgroundMusic = value; }
+        }
+    }
+}
